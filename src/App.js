467ad465be4fc6d22dff6d921d1e5d9bc6ff27c8
Ibrahim AlRayyan <ibrahim.alrayany@gmail.com>
2023-06-16 11:45:09 +0300

    Context API with retail store - Add to cart, add to favorites and show details actions. In addirion to useReducer actions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
-import DrinkSearch from "./drink-search/drink-search";
 import { Container } from "reactstrap";
+import Retail from "./retail-store/retail";
+import { RetailProvider } from "./retail-store/retail-context";
+import fakeProducts from "./retail-store/fake-products";
 
 function App() {
   return (
     <Container>
-      <DrinkSearch />
+      <RetailProvider products={fakeProducts}>
+        <Retail />
+      </RetailProvider>
     </Container>
   );
 }
